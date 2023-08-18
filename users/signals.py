7d1f0bea9d7e5b9015d9b75546a71d956b2d8dc9
atmaniali <atmaniali97@gmail.com>
2023-08-18 17:29:50 +0100

    add signals to generate Token automatically

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 from .models import Profile
 
@@ -8,3 +9,9 @@ from .models import Profile
 def create_profile_user(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
